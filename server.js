dbdd0060d7eda685ef591e5cadaf19748dd20865
amosmoyo <amosmoyo5300@gmail.com>
2021-11-03 00:35:20 +0300

    addn some securities packages

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
 const adminRouter = require('./routes/admin');
 
+// sanitization
+const mongoSanitize = require('express-mongo-sanitize');
+
+// security header protection
+const helmet = require("helmet");
+
+// compress request
+const compression = require('compression');
+
+// cross-site scripting
+const xss = require('xss-clean')
+
+// rate limit and hpp
+const hpp = require('hpp');
+const rateLimit = require("express-rate-limit");
+
 dotenv.config({path: './config/config.env'});
 
 // initialize database connection
@@ -20,6 +36,30 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// To remove data, use:
+app.use(mongoSanitize());
+
+//  X-XSS-Protection
+app.use(helmet())
+
+// Prevent cross-site scripting
+app.use(xss())
+
+// compress request to reduce load time
+app.use(compression())
+
+// protect against HTTP Parameter Pollution attacks
+app.use(hpp())
+
+// rate limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
 
 // routes
 app.use('/api/v1/auth', authRouter);
