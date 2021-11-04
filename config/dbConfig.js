3deabae23f076e45f3514cce96ac2ee2256be214
amosmoyo <amosmoyo5300@gmail.com>
2021-11-04 17:53:00 +0300

    modified the config/config.env

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const dbConnct = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URL, {
+  const conn = await mongoose.connect(`mongodb+srv://${process.env.MONGODB_URL_USER}:${process.env.MONGODB_URL_PASS}@cluster0.spg3d.mongodb.net/askehealthcare?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
